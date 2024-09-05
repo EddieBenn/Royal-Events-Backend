d67467697f391aeb93a346708c997716d088b29f
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-09-05 10:22:05 +0100

    fixes made to run production instance permanently

--- src/configurations/config.ts
+++ src/configurations/config.ts
@@ -6,11 +6,13 @@ dotenv.config()
 const stage: any = process.env.NODE_ENV;
 let config
 
-if(stage === "development"){
-    config = require("./dev").default
-}else if(stage === "production"){
-    config = require("./prod").default
-}
+// if(stage === "development"){
+//     config = require("./dev").default
+// }else if(stage === "production"){
+//     config = require("./prod").default
+// }
+
+config = require("./prod").default
 
 export default merge({
     stage
